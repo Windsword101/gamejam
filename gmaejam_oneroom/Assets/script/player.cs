001fb67777521954eea54fb145d6aea5e35fe883
Windsword101 <63327487+Windsword101@users.noreply.github.com>
2020-06-19 16:09:43 +0800

    playercode

---
+++ gmaejam_oneroom/Assets/script/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public int hunger = 100;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = ("飢餓值:" + hunger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
